5bbdf7bd6bce7006be78fc8adc0c241f3165aed7
Gui Rios <guilhermeriosprog@gmail.com>
2025-07-12 16:43:34 -0300

    fix:corrigindo bug de deploy

--- src/app/(private_pages)/complaintDetails/[id]/page.tsx
+++ src/app/(private_pages)/complaintDetails/[id]/page.tsx
@@ -1,10 +1,10 @@
 import ComplaintDetailsServer from "@/app/components/complaint-details/server";
 
-export default async function ComplaintDetailsPage({
-  params,
-}: {
+interface PageProps {
   params: { id: string };
-}) {
+}
+
+export default async function ComplaintDetailsPage({ params }: PageProps) {
   return (
     <div className="p-8">
       <ComplaintDetailsServer id={params.id} />
