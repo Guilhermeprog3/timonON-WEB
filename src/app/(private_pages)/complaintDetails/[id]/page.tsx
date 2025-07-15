60dfae1452cf2f45cb1db9f35a862cf807e68572
Gui Rios <guilhermeriosprog@gmail.com>
2025-07-15 11:00:22 -0300

    remove:removendo alerts

--- src/app/(private_pages)/complaintDetails/[id]/page.tsx
+++ src/app/(private_pages)/complaintDetails/[id]/page.tsx
@@ -1,12 +1,12 @@
 import ComplaintDetailsServer from "@/app/components/complaint-details/server";
 
-type ComplaintDetailsPageProps = {
-  params: {
-    id: string;
-  };
+// Definindo o tipo para as props da página de forma mais completa
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default async function ComplaintDetailsPage({ params }: ComplaintDetailsPageProps) {
+export default async function ComplaintDetailsPage({ params }: Props) {
   return (
     <div className="p-8">
       <ComplaintDetailsServer id={params.id} />
